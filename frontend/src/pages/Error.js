d012c88b228b61108b06c0489af6c73752c19c21
Destiny Boone <destaj.db@gmail.com>
2023-04-13 15:22:12 -0400

    extracting error data and throwing responses

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,30 @@
-function ErrorPage(){
-    return <h1>An error occurred!</h1>
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
+
+function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 400) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
+  return (
+    <>
+      <MainNavigation></MainNavigation>
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
 
 export default ErrorPage;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -10,11 +10,14 @@ function EventsPage() {
 
 export default EventsPage;
 
-export async function loader() { // could access local storage here, cookies, etc.; cannot use hooks in here though because this is just regular javascript, not react
+export async function loader() {
+  // could access local storage here, cookies, etc.; cannot use hooks in here though because this is just regular javascript, not react
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw {message: 'Could not fetch events.'}; // this will reach the errorElement attribute established in our myRouter var in App.js
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    }); // this will reach the errorElement attribute established in our myRouter var in App.js
   } else {
     return response; // whatever returned in loader will be made available for that page and any other components where you need it
   }
