e3c84ef596e705c724a23a22fee181f7a3a27db3
Destiny Boone <destaj.db@gmail.com>
2023-04-13 15:26:35 -0400

    the json() utility function

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,5 @@
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 
 function EventsPage() {
   const data = useLoaderData(); // grabs data returned by loader in App.js
@@ -15,9 +15,11 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-      status: 500,
-    }); // this will reach the errorElement attribute established in our myRouter var in App.js
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    // this will reach the errorElement attribute established in our myRouter var in App.js
+    throw json({ message: "Could not fetch events." }, { status: 500 }); // easiest to write like this, and you won't have to parse it in your Error.js
   } else {
     return response; // whatever returned in loader will be made available for that page and any other components where you need it
   }
