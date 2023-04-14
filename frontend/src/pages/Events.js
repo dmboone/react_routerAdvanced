f8eb0496644a8cb47172dbf10db68e62db6e0ace
Destiny Boone <destaj.db@gmail.com>
2023-04-14 13:13:46 -0400

    deferring data fetching with defer()

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,16 +1,21 @@
+import { Suspense } from "react";
 import EventsList from "../components/EventsList";
-import { useLoaderData, json } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 
 function EventsPage() {
-  const data = useLoaderData(); // grabs data returned by loader in App.js
-  const myEvents = data.events;
-
-  return <>{<EventsList events={myEvents} />}</>;
+  const { events } = useLoaderData(); // grabs data returned by loader in App.js
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents}></EventsList>}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   // could access local storage here, cookies, etc.; cannot use hooks in here though because this is just regular javascript, not react
   const response = await fetch("http://localhost:8080/events");
 
@@ -21,6 +26,13 @@ export async function loader() {
     // this will reach the errorElement attribute established in our myRouter var in App.js
     throw json({ message: "Could not fetch events." }, { status: 500 }); // easiest to write like this, and you won't have to parse it in your Error.js
   } else {
-    return response; // whatever returned in loader will be made available for that page and any other components where you need it
+    const resData = await response.json();
+    return resData.events; // whatever returned in loader will be made available for that page and any other components where you need it
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
